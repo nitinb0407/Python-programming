9f11bab78393ca0c45ca6552c8372321ae0ade99
nitinb0407 <nitinb0407@gmail.com>
2026-09-03 14:37:10 +0530

    Add Module 1 Lab Exercise 1 basic Python program

---
+++ MODULE 1/lab_exercise_1_basic_python.py
@@ -0,0 +1,8 @@
+# MODULE 1 - Lab Exercise 1
+# Basic Python Program
+
+print("Hello, World!")
+print("Welcome to Python Programming")
+
+name = input("Enter your name: ")
+print("Hello", name + "!")
