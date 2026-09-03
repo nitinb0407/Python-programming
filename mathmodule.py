fce08075add2688d1de4c398906f80157f306ea9
nitinb0407 <nitinb0407@gmail.com>
2026-09-03 12:13:44 +0530

    Add files via upload

---
+++ mathmodule.py
@@ -0,0 +1,10 @@
+import math
+a=2
+print(math.pow(a,2))
+print(math.pow(a,3))
+print(math.sqrt(a))
+print(math.ceil(2.01))
+print(math.floor(2.02))
+print(math.fabs(-10))
+print(type(a))
+print(id(a))
