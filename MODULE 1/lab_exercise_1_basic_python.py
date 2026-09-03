56816fe4eef0314434207f2ca87e0c676d75e79f
nitinb0407 <nitinb0407@gmail.com>
2026-09-03 14:44:33 +0530

    Delete MODULE 1 directory

--- MODULE 1/lab_exercise_1_basic_python.py
+++
@@ -1,8 +0,0 @@
-# MODULE 1 - Lab Exercise 1
-# Basic Python Program
-
-print("Hello, World!")
-print("Welcome to Python Programming")
-
-name = input("Enter your name: ")
-print("Hello", name + "!")
